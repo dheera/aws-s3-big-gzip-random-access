9691271fc7f2643fbcc58467606005a6baa5fd91
Dheera <dheera@dheera.net>
2025-02-05 15:35:58 +0000

    fixes

---
+++ example.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import os
+import boto3
+import json
+import io
+import re
+
+from boto3.s3.transfer import TransferConfig
+from botocore.config import Config
+import indexed_gzip # pip3 install indexed_gzip
+
+from seekable_s3_stream import SeekableS3Stream
+from index import build_index_for_file, get_stream_for_file
+
+KB = 2 ** 10
+MB = 2 ** 20
+GB = 2 ** 30
+
+# Step 0: Build the index
+
+# This example is for building an index for a 100GB+ .csv.gz file from Polygon
+# This example requires a paid subscription to the "Options Advanced" credentials
+# but you can replace it with whatever .gz dataset you have access to
+
+# You can build the indexes on an EC2 instance which should have free downlink bandwidth
+# The indexes will be output to .gzidx/
+
+with open("polygon.json", "r") as f:
+    config = json.load(f)
+
+session = boto3.Session(
+   aws_access_key_id=config["s3_access_key_id"],
+   aws_secret_access_key=config["s3_secret_access_key"],
+)
+
+s3 = session.client("s3", endpoint_url=config["s3_endpoint"], config=Config(signature_version='s3v4'))
+bucket_name = "flatfiles"
+object_key = "us_options_opra/quotes_v1/2025/01/2025-01-28.csv.gz" # massive 100GB+ file
+
+build_index(s3, bucket_name, object_key)
+
+# Step 1: Random access
+
+# This can be done from anywhere even with a low bandwidth connection.
+# You just need to transfer the index directory .gzidx/ to wherever you plan to access from
+
+stream = get_sream(s3, bucket_name, object_key)
+
+stream.seek(53 * GB) # seek to a location 53GB into the file to demonstrate random access
+stream.readline() # throwaway the first partial CSV line
+
+for i in range(20): # read 20 more lines
+    print(stream.readline())
+
+
---
+++ index.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import os
+import boto3
+import json
+import io
+import re
+
+from boto3.s3.transfer import TransferConfig
+from botocore.config import Config
+import indexed_gzip # pip3 install indexed_gzip
+
+from seekable_s3_stream import SeekableS3Stream
+
+def build_index(s3_client, bucket_name, object_key):
+    assert(object_key.endswith(".gz"))
+
+    host = "__unknown_host__"
+    try:
+        host = s3._endpoint.host
+        match = re.match(r"^(?:https?:\/\/)?([^\/]+)", host)
+        host = match.group(1)
+    except:
+        print("Warning: failed to extract hostname from s3 client, maybe they changed the internal class structure")
+
+    # Create seekable stream
+    seekable_stream = SeekableS3Stream(s3_client, bucket_name, object_key)
+
+    # Use with indexed_gzip
+    with indexed_gzip.IndexedGzipFile(fileobj=seekable_stream, spacing = 32*MB, readbuf_size = 16*MB) as fobj:
+        fobj.build_full_index()
+        destination_path = os.path.join(".gzidx", host, bucket_name, object_key.replace(".gz", ".gzidx"))
+        destination_dir = os.path.split(destination_path)[0]
+        os.makedirs(destination_dir, exist_ok = True)
+        fobj.export_index(destination_path)
+
+def build_index_for_prefix(s3_client, bucket_name, in_prefix):
+    paginator = s3_client.get_paginator('list_objects_v2')
+    indexables = []
+    for page in paginator.paginate(Bucket=bucket_name, Prefix=in_prefix):
+        for obj in page['Contents']:
+            if obj['Key'].endswith('.gz'):
+                indexables.append(obj['Key'])
+
+    for indexable in indexables:
+        print(f"Building index for {indexable}")
+        build_index(s3_client, bucket_name, in_prefix)
+
+def get_stream(s3_client, bucket_name, object_key, indenx_file = None):
+    assert(object_key.endswith(".gz"))
+
+    host = "__unknown_host__"
+    try:
+        host = s3._endpoint.host
+        match = re.match(r"^(?:https?:\/\/)?([^\/]+)", host)
+        host = match.group(1)
+    except:
+        print("Warning: failed to extract hostname from s3 client, maybe they changed the internal class structure")
+
+    # Create seekable stream
+    seekable_stream = SeekableS3Stream(s3_client, bucket_name, object_key)
+    
+    if index_file is None: # assume it is where we would have created it with the build_index() function
+        index_path = os.path.join(".gzidx", host, bucket_name, object_key.replace(".gz", ".gzidx"))
+    
+    assert(os.path.exists(index_path))
+
+    return indexed_gzip.IndexedGzipFile(fileobj=seekable_stream, index_file=index_path)
+
