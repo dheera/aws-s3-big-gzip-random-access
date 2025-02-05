b307067cf11e9a250501198cf7eba17821edc63a
dheera <dheera@dheera.net>
2025-02-05 15:17:25 +0000

    fixes

--- build_index.py
+++ build_index.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 import boto3
 import json
 import io
+import re
+
 from boto3.s3.transfer import TransferConfig
 from botocore.config import Config
 import indexed_gzip # pip3 install indexed_gzip
 
-from seeakable_s3_stream import SeekableS3Stream
+from seekable_s3_stream import SeekableS3Stream
 
 # Load S3 Config
 with open("polygon.json", "r") as f:
@@ -18,22 +21,29 @@ session = boto3.Session(
    aws_secret_access_key=config["s3_secret_access_key"],
 )
 
-s3 = boto3.client("s3", endpoint_url=config["s3_endpoint"], config=Config(signature_version='s3v4'))
-bucket_name = "flatfiles"
-object_key = "us_options_opra/quotes_v1/2025/01/2025-01-28.csv.gz"
+def build_index_for_file(in_s3_client, in_bucket_name, in_object_key):
+    assert(in_object_key.endswith(".gz"))
 
-def build_index_for_file(in_s3_client, in_bucket_name, in_object_key, out_s3_client, out_bucket_name, out_object_key):
-    assert(in_object_key.endswith(".gz")
+    host = "__unknown_host__"
+    try:
+        host = s3._endpoint.host
+        match = re.match(r"^(?:https?:\/\/)?([^\/]+)", host)
+        host = match.group(1)
+    except:
+        print("Warning: failed to extract hostname from s3 client, maybe they changed the internal class structure")
 
     # Create seekable stream
     seekable_stream = SeekableS3Stream(in_s3_client, in_bucket_name, in_object_key)
 
     # Use with indexed_gzip
-    with indexed_gzip.IndexedGzipFile(fileobj=seekable_stream) as f:
+    with indexed_gzip.IndexedGzipFile(fileobj=seekable_stream) as fobj:
         fobj.build_full_index()
-        fobj.export_index(bucket_name + "-" + object_key.replace(".gz", ".gzidx").replace("/", "-"))
+        destination_path = os.path.join(host, bucket_name, object_key.replace(".gz", ".gzidx"))
+        destination_dir = os.path.split(destination_path)[0]
+        os.makedirs(destination_dir, exist_ok = True)
+        fobj.export_index(destination_path)
 
-def build_index_for_prefix(in_s3_client, in_bucket_name, in_prefix, out_s3_client, out_bucket_name, out_prefix):
+def build_index_for_prefix(in_s3_client, in_bucket_name, in_prefix):
     paginator = in_s3_client.get_paginator('list_objects_v2')
     indexables = []
     for page in paginator.paginate(Bucket=in_bucket_name, Prefix=in_prefix):
@@ -43,4 +53,10 @@ def build_index_for_prefix(in_s3_client, in_bucket_name, in_prefix, out_s3_clien
 
     for indexable in indexables:
         print(f"Building index for {indexable}")
-        build_index_for_file(in_s3_client, in_bucket_name, in_prefix, None, None, None)
+        build_index_for_file(in_s3_client, in_bucket_name, in_prefix)
+
+s3 = session.client("s3", endpoint_url=config["s3_endpoint"], config=Config(signature_version='s3v4'))
+bucket_name = "flatfiles"
+object_key = "us_options_opra/quotes_v1/2025/01/2025-01-28.csv.gz"
+
+build_index_for_file(s3, bucket_name, object_key)
