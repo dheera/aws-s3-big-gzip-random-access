f8c09b967795b5a2b107db08da8cfba2cdfdd923
dheera <dheera@dheera.net>
2025-02-08 06:28:39 -0800

    example

--- example_singlefile.py
+++ example_singlefile.py
@@ -15,13 +15,9 @@ MB = 2 ** 20
 GB = 2 ** 30
 
 # Step 0: Build the index
-
 # This example is for building an index for a 100GB+ .csv.gz file from Polygon
 # This example requires a paid subscription to the "Options Advanced" credentials
 # but you can replace it with whatever .gz dataset you have access to
-
-# You can build the indexes on an EC2 instance which should have free downlink bandwidth
-# The indexes will be output to .gzidx/
 
 with open("polygon.json", "r") as f:
     config = json.load(f)
@@ -39,10 +35,11 @@ object_key = "us_options_opra/quotes_v1/2025/01/2025-01-28.csv.gz" # 100GB+ larg
 # this will take a while
 build_index(s3, bucket_name, object_key)
 
-# Step 1: Random access
+# the resulting index that enables random access will be in .gzidx/
+# you can copy this to wherever else you want before doing Step 1 below
 
+# Step 1: Random access
 # This can be done from anywhere even with a low bandwidth connection.
-# You just need to transfer the index directory .gzidx/ to wherever you plan to access from
 
 stream = get_stream(s3, bucket_name, object_key)
 
